81638bd673cb5e99df60ca2492f0a831d7d5bca7
Evan Lee <evan.lee.615@gmail.com>
2024-04-14 19:48:20 -0400

    added clear all button

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function fetchData () {
             let state = e.target.value
             stateArray.push(state)
 
-            //array iteration
+            //Array iteration
             data.find(function (element) {
                 if(element.type === cancerArray[cancerArray.length - 1]) {
                     document.getElementById('death-count').textContent = `Death Count: ${element[state].deathCount}`
@@ -29,55 +29,35 @@ function fetchData () {
             })
         })
         
-                    //Event listener
-                    document.getElementById('comparison-button').addEventListener('click', function() {
-                        let deathCountStr = document.getElementById('death-count').textContent
-                        deathCountStr = deathCountStr.substring(13)
+        //Event listener
+        document.getElementById('comparison-button').addEventListener('click', function() {
+            let deathCountStr = document.getElementById('death-count').textContent
+            deathCountStr = deathCountStr.substring(13)
 
-                        let incidenceCountStr = document.getElementById('incidence-count').textContent
-                        incidenceCountStr = incidenceCountStr.substring(17)
+            let incidenceCountStr = document.getElementById('incidence-count').textContent
+            incidenceCountStr = incidenceCountStr.substring(17)
 
-                        let mortalityRateStr = document.getElementById('mortality-rate').textContent
-                        mortalityRateStr = mortalityRateStr.substring(16)
+            let mortalityRateStr = document.getElementById('mortality-rate').textContent
+            mortalityRateStr = mortalityRateStr.substring(16)
 
-                        let newTR = document.createElement('tr')
-                        newTR.innerHTML =
-                            `<td class='comparison-data'>${cancerArray[cancerArray.length - 1]}</td>
-                            <td class='comparison-data'>${stateArray[stateArray.length - 1]}</td>
-                            <td class='comparison-data'>${deathCountStr}</td>
-                            <td class='comparison-data'>${incidenceCountStr}</td>
-                            <td class='comparison-data'>${mortalityRateStr}</td>`
-                        document.getElementById('comparison-table').append(newTR)
-                    })   
-                }
-            )
-        }
-
+            let newTR = document.createElement('tr')
+            newTR.innerHTML =
+                `<td class='comparison-data'>${cancerArray[cancerArray.length - 1]}</td>
+                <td class='comparison-data'>${stateArray[stateArray.length - 1]}</td>
+                <td class='comparison-data'>${deathCountStr}</td>
+                <td class='comparison-data'>${incidenceCountStr}</td>
+                <td class='comparison-data'>${mortalityRateStr}</td>`
+            document.getElementById('comparison-table').append(newTR)
+        })
+        
+        document.getElementById('clear-table').addEventListener('click', function() {
+            let tableData = document.getElementsByClassName('comparison-data')
+            let tableData2 = [...tableData]
+            tableData2.forEach(function(item){
+                item.innerText = ``
+            })
+        })
+    })
+}
         
 fetchData()
-
-
-// let newTD1 = document.createElement('td')
-                            // newTD1.setAttribute('class','comparison-data')
-                            // newTR.append(newTD1)
-                            // newTD1.textContent = cancerType
-        
-                            // let newTD2 = document.createElement('td')
-                            // newTD2.setAttribute('class','comparison-data')
-                            // newTR.append(newTD2)
-                            // newTD2.textContent = state
-                            
-                            // let newTD3 = document.createElement('td')
-                            // newTD3.setAttribute('class','comparison-data')
-                            // newTR.append(newTD3)
-                            // newTD3.textContent = `${element[state].deathCount}`
-                            
-                            // let newTD4 = document.createElement('td')
-                            // newTD4.setAttribute('class','comparison-data')
-                            // newTR.append(newTD4)
-                            // newTD4.textContent = `${element[state].incidenceCount}`
-        
-                            // let newTD5 = document.createElement('td')
-                            // newTD5.setAttribute('class','comparison-data')
-                            // newTR.append(newTD5)
-                            // newTD5.textContent = `${element[state].mortalityRate}%`
