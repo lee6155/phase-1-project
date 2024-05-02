73c3e2613e603ed7fbb5d5ff8be2e5bf1c15c3d2
Evan Lee <evan.lee.615@gmail.com>
2024-05-01 23:42:18 -0400

    this version ready for assessment

--- index.js
+++ index.js
@@ -69,3 +69,7 @@ document.getElementById('clear-table').addEventListener('click', function() {
 
 fetchData()
 
+
+
+//let stateArray = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho','Illinois','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
